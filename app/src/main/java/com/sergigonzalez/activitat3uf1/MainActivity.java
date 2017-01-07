3191e836aeedf867548b2a72cb89a64f57d1ae6e
SergiGonzalezDAM <job_sergi@hotmail.es>
2017-01-07 21:43:45 +0100

    Arreglado SharedPreferences e implementado cambio de color, fuente y mensaje de bienvenida

--- app/src/main/java/com/sergigonzalez/activitat3uf1/MainActivity.java
+++ app/src/main/java/com/sergigonzalez/activitat3uf1/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -22,13 +24,15 @@ public class MainActivity extends AppCompatActivity
         db = dataBase.getWritableDatabase();
 
         SharedPreferences prefs =
-                getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+                getSharedPreferences("CalendarioDAM", Context.MODE_PRIVATE);
 
         String name = prefs.getString("nombre", "Nombre");
         String colour = prefs.getString("colour", "Blanco");
         String group = prefs.getString("group", "A1");
         String font = prefs.getString("font", "Sans-serif");
-        System.out.println(colour);
+        TextView textoBienvenida = (TextView) findViewById(R.id.textview);
+        textoBienvenida.setText("Bienvenido/a " + name);
+
         switch (colour)
         {
             case "Blanco":
@@ -39,6 +43,18 @@ public class MainActivity extends AppCompatActivity
                 break;
             case "Verde" :
                 findViewById(R.id.activity_main).setBackgroundColor(Color.GREEN);
+                break;
+        }
+        switch (font)
+        {
+            case "Sans-serif":
+                textoBienvenida.setTypeface(Typeface.SANS_SERIF);
+                break;
+            case "Serif":
+                textoBienvenida.setTypeface(Typeface.SERIF);
+                break;
+            case "Monospace":
+                textoBienvenida.setTypeface(Typeface.MONOSPACE);
                 break;
         }
 
